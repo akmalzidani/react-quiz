72780aae27ddd74f2124f0fb2186d6e61a58e594
akmalzidani <akmal20031003@gmail.com>
2024-10-07 17:32:20 +0700

    setup reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+const initialState = {
+  questions: [],
+  currentQuestion: 0,
+  score: 0,
+
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "nextQuestion":
+      return { ...state, currentQuestion: state.currentQuestion + 1 };
+    case "incrementScore":
+      return { ...state, score: state.score + 1 };
+    default:
+      throw new Error("Invalid action type");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchApi() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (error) {
+        dispatch({ type: "dataFailed" });
+        console.error(error);
+      }
+    }
+    fetchApi();
+    return () => {};
+  }, []);
+
   return (
     <div className="app">
       <Header />
